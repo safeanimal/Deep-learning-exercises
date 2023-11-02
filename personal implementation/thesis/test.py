7eb27d6ecb7d009a2fa892421c01108b0029959e
Guorui Sang <411814789@qq.com>
2023-11-02 16:31:05 +0800

    测试模块添加自动处理多个图片集的逻辑

--- personal implementation/thesis/test.py
+++ personal implementation/thesis/test.py
@@ -1,75 +1,84 @@
-import argparse
-
+import os
 import yaml
-
-import utils
+from metric import peak_signal_to_noise_ratio
 from PIL import Image
-from model2 import SuperResolutionModel
+from model0 import SuperResolutionModel
 import torchvision.transforms as tvt
 import torchvision.transforms.functional as tvf
 import torch
 import copy
 from torchvision.utils import save_image
+from logger import AverageLogger
+import datetime
 
-parser = argparse.ArgumentParser(description='Test')
-parser.add_argument('input_image_path', nargs='?', type=str,
-                    default='D:/Materials/dataset/DIV2K 2018/DIV2K_valid_LR_unknown_X4_sub/')
-parser.add_argument('input_image_name', nargs='?', type=str, default='0831_s013.png')
-parser.add_argument('target_image_path', nargs='?', type=str, default='test')
-parser.add_argument('state_dict_path', nargs='?', default='save/best.pth')
-args = parser.parse_args()
-
-with open('config.yml', 'r', encoding='utf-8') as file:
+with open('test_config.yml', 'r', encoding='utf-8') as file:
     config = yaml.safe_load(file)
+
+test_dirs = config['test_dirs']
+test_type = config['test_type']
+
+log_path = config['log_path']
+
+model_parameters_path = config['model_parameters_path']
 
 # 选择GPU为device
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-hyperparameters_config = config['hyperparameters']
-model_num = hyperparameters_config['model_num']
+model = SuperResolutionModel()
+model.load_state_dict(torch.load(model_parameters_path))
 
-# 加载模型
-model_parameters = config['saved_model'+str(model_num)]
-image_size = model_parameters['image_size']
-if model_num == 0:
-    num_blocks = model_parameters['num_blocks']
-    model = SuperResolutionModel(num_blocks=num_blocks, image_size=image_size).to(device)
-elif model_num == 1:
-    in_channels = model_parameters['in_channels']
-    out_channels_after_conv1 = model_parameters['out_channels_after_conv1']
-    out_channels_after_conv2 = model_parameters['out_channels_after_conv2']
-    patch_sizes = model_parameters['patch_sizes']
-    num_blocks = model_parameters['num_blocks']
-    upscale_factor = model_parameters['upscale_factor']
-    model = SuperResolutionModel(in_channels=in_channels, out_channels_after_conv1=out_channels_after_conv1,
-                                 out_channels_after_conv2=out_channels_after_conv2, patch_sizes=patch_sizes,
-                                 num_blocks=num_blocks, upscale_factor=upscale_factor).to(device)
-elif model_num == 2:
-    model = SuperResolutionModel().to(device)
-
-model.load_state_dict(torch.load(args.state_dict_path))
-
+# 开启推理模式
 model.eval()
-
-original_image = Image.open(args.input_image_path+args.input_image_name)
 
 input_transform = tvt.Compose([
     tvt.ToTensor()
 ])
 
-# 图像预处理
-original_image = input_transform(original_image)
-lr_image = copy.deepcopy(original_image)
-lr_image = lr_image.to(device)
-lr_image = lr_image.reshape(1, *lr_image.shape)
-hr_image = model(lr_image)
+# 创建记录器
+avg_logger = AverageLogger()
 
-lr_image = lr_image.cpu()
-hr_image = hr_image.cpu()
-utils.show_images_tensor(1, lr_image)
-utils.show_images_tensor(1, hr_image)
+if not os.path.exists(log_path):
+    os.mkdir(log_path)
 
+with open('test_log.txt', mode='a', encoding='uft-8') as file:
+    file.writelines('Date:', datetime.datetime.now())
+    # 以此测试每一个图片集
+    for test_dir in test_dirs:
+        image_names = os.listdir(test_dir)
+        LR_image_paths = [os.path.join(test_dir, image_name) for image_name in image_names if image_name[-6:-4] == test_type]
+        GT_image_paths = [os.path.join(test_dir, image_name) for image_name in image_names if image_name[-6:-4] == 'HR']
+        print(LR_image_paths)
+        print(GT_image_paths)
+        for i in range(len(LR_image_paths)):
+            lr_image = Image.open(LR_image_paths[i]).convert('RGB')
+            lr_image = input_transform(lr_image).to(device)
 
-save_image(lr_image, 'test/num8/lr_image_'+args.input_image_name+'.png')
-save_image(hr_image, 'test/num8/hr_image_'+args.input_image_name+'.png')
-save_image(original_image.squeeze(0), 'test/num8/original_image_'+args.input_image_name+'.png')
+            gt_image = Image.open(GT_image_paths[i]).convert('RGB')
+            gt_image = input_transform(gt_image).to(device)
+
+            hr_image = model(lr_image)
+
+            psnr = peak_signal_to_noise_ratio(hr_image, gt_image)
+            avg_logger.update(psnr)
+
+        file.writelines(f'For {test_dir} \n\tPSNR (avg):{avg_logger.avg}\n')
+        avg_logger.clear()
+
+# original_image = Image.open(args.input_image_path+args.input_image_name)
+#
+# # 图像预处理
+# original_image = input_transform(original_image)
+# lr_image = copy.deepcopy(original_image)
+# lr_image = lr_image.to(device)
+# lr_image = lr_image.reshape(1, *lr_image.shape)
+# hr_image = model(lr_image)
+#
+# lr_image = lr_image.cpu()
+# hr_image = hr_image.cpu()
+# utils.show_images_tensor(1, lr_image)
+# utils.show_images_tensor(1, hr_image)
+#
+#
+# save_image(lr_image, 'test/num8/lr_image_'+args.input_image_name+'.png')
+# save_image(hr_image, 'test/num8/hr_image_'+args.input_image_name+'.png')
+# save_image(original_image.squeeze(0), 'test/num8/original_image_'+args.input_image_name+'.png')
